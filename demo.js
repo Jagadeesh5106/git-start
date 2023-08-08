f22fe6d517719a916dd12bcf5b7adc922135ab84
Jagadeesh5106 <narayanamjagadeesh555@gmail.com>
2023-08-08 19:35:26 +0530

    GetElementsByTagName

--- demo.js
+++ demo.js
@@ -15,16 +15,30 @@ var headerTitle=document.getElementById("header-title")
 // headerTitle.textContent='Hello';
 headerTitle.style.borderBottom='3px solid black'
 
+
+
+//GetElementByTagName
+
+var li=document.getElementsByTagName('li');
+
+li[2].style.backgroundColor='green';
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight='bold';
+    li[i].style.color='blue';
+}
+
 //GetElementByClassName
 var addItems=document.getElementsByClassName("title")
 console.log(addItems)
 addItems[0].style.color="green";
 addItems[0].style.fontWeight="bold";
 var items=document.getElementsByClassName('list-group-item');
-console.log(items)
+for(var i of items){
+    console.log(i);
+}
+
 items[2].style.backgroundColor='green';
 for(var i=0;i<items.length;i++){
     items[i].style.fontWeight='bold';
-    items[i].style.color='blue';
+    items[i].style.color='black';
 }
-
